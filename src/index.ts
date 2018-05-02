db4a7dd7efe275638e538c6a23ef8854a9a16fda
bmaximilian <maximilianbmail@gmail.com>
2018-05-02 11:13:21 +0200

    Added object functions to export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { blacklist } from './objects/blacklist';
 import { defineBlacklist } from './objects/defineBlacklist';
 import { defineWhitelist } from './objects/defineWhitelist';
+import { filterForProperty } from './objects/filterForProperty';
+import { hasMatchingKeys } from './objects/hasMatchingKeys';
+import { objectCamelCaseToLowDash } from './objects/objectCamelCaseToLowDash';
+import { objectLowDashToCamelCase } from './objects/objectLowDashToCamelCase';
 import { whitelist } from './objects/whitelist';
 import { camelCaseToLowDash } from './strings/camelCaseToLowDash';
 import { formatGetUrlParameters } from './strings/formatGetUrlParameters';
@@ -15,5 +19,9 @@ export {
     blacklist,
     defineBlacklist,
     defineWhitelist,
+    filterForProperty,
+    hasMatchingKeys,
+    objectCamelCaseToLowDash,
+    objectLowDashToCamelCase,
     whitelist,
 };
